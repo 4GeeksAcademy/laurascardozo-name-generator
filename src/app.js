9867161f24bffda5830f34605e172b5a6d50313e
Laurascardozo <143517864+Laurascardozo@users.noreply.github.com>
2023-10-19 02:16:39 +0000

    domainNames generator created

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let tlds = [".com", ".net"];
+
+  let domainNames = [];
+
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
+          domainNames.push(`${pronoun}${adj}${noun}${tld}`);
+        }
+      }
+    }
+  }
+
+  document.body.innerHTML = `<ul> ${domainNames.map(
+    domainName => `<li>${domainName}</li>`
+  )}</ul>`;
 };
